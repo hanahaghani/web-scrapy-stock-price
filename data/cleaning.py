10b5655429898bbddf9b97c9eae180e66346bd37
hanahaghani <hanahaghani6@gmail.com>
2026-08-17 12:35:16 +0330

    data redy for atomaition

--- data/cleaning.py
+++ data/cleaning.py
@@ -4,16 +4,19 @@ import pandas as pd
 base_dir=Path(__file__).resolve().parent
 data=pd.read_csv(base_dir/"raw"/"price.csv")
 data.info()
-
-price_columns = ["ons_gold","silver","coin","gold_18k"]
+print(data.head())
+price_columns = ["ons_gold","silver","coin"]
 
 for column in price_columns:
+    data[column]=data[column].str.replace(',','',regex=False)
     data[column]=pd.to_numeric(data[column],errors='coerce')
     print(data.info())
 
+data['timestamp']=data['timestamp'].str.replace(',','',regex=False)
 data['timestamp']=pd.to_datetime(data['timestamp'],errors='coerce')
 
 data.info()
+print(data.head())
 
 output_path=base_dir/"processed"/'price-final.csv'
 data.to_csv(output_path,index=False)
--- web_scraper/pipelines.py
+++ web_scraper/pipelines.py
@@ -6,8 +6,48 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import csv
+from pathlib import Path
 
 
 class WebScraperPipeline:
-    def process_item(self, item):
+    def open_spider(self,spider):
+        self.file_path = Path("data/raw/price.csv")
+
+        self.file_path.parent.mkdir(parents=True, exist_ok=True)
+
+        file_exists = (
+            self.file_path.exists()
+            and self.file_path.stat().st_size > 0
+        )
+
+        self.file = open(
+            self.file_path,
+            "a",
+            newline="",
+            encoding="utf-8"
+        )
+
+        self.writer = csv.DictWriter(
+            self.file,
+            fieldnames=[
+                "timestamp",
+                "gold_18k",
+                "ons_gold",
+                "silver",
+                "coin"
+            ]
+        )
+
+        if not file_exists:
+            self.writer.writeheader()
+
+
+    def process_item(self, item,spider):
+        self.writer.writerow(dict(item))
+        
         return item
+
+    def close_spider(self,spider):
+           self.file.close()
+   
